6911ee1ea18f330b3a1290217c24f3246aa1091b
Devanshu Tiwaskar <devanshutiwaskar@gmail.com>
2025-09-18 16:32:59 +0530

    Fix API client configuration by correcting baseURL and ensuring proper axios instance creation

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: 'http://localhost:3000',
+const api = axicomos.create({
+  baseURL: "https://food-delivery-s3fj.onrender.",
 //   baseURL: "https://ecomm-auth-user-and-seller.onrender.com/api",
   withCredentials: true,
   headers: {
